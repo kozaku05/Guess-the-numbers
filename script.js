698d7d8a4095806923530e8a706cd7a822c3fe56
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-01-12 10:26:06 +0900

    Update script.js
    toggleCutomにと関する変更

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 localStorage.removeItem("userReqest");
 
+// toggleCutomへ名称変更
 const toggleCustom = () => 
-  document.querySelectorAll("#custom-maxnum, #custom-move, button").forEach(el => el.classList.toggle("show"));
+  document.querySelectorAll("#custom-maxnum, #custom-maxcon, #submit").forEach(el => el.classList.toggle("show"));
 
+// mapでの取り扱いに変更
 const submitCustom = () => {
-  const [maxNum, moves] = ["custom-maxnum", "custom-move"].map(id => document.getElementById(id).value);
-  if (maxNum && moves) {
-    localStorage.setItem("userReqest", JSON.stringify({ maxNum, maxMove: moves }));
+  const [maxNum, maxCon] = ["custom-maxnum", "custom-maxcon"].map(id => document.getElementById(id).value);
+  if (maxNum && maxCon) {
+    localStorage.setItem("userReqest", JSON.stringify({ maxNum, maxCon }));
     location.href = "./main/custom/custom.html";
   } else {
     alert("数値を入力してください");
